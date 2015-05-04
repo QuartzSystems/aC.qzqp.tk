666b2b4ef7beee585ae19ac8150153957b070417
qsysmine <echomanari@gmail.com>
2015-05-04 13:42:22 +0000

    Initial Deployment

--- se/server.js
+++ se/server.js
@@ -136,7 +136,7 @@ app.use(function(err, req, res, next) {
     next();
 });
 //LISTEN
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
